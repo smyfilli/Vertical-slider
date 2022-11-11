940da5fa3a7a2ddd23bd1cf44d57e09593b0f251
smyfilli <107491885+smyfilli@users.noreply.github.com>
2022-11-11 12:09:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const container = document.querySelector('.container')
+const mainSlide = document.querySelector('.main-slide')
+const slidesCount = mainSlide.querySelectorAll('div').length
+
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidesCount - 1)*100}vh`
+
+upBtn.addEventListener('click', ()=>{
+    changeSlide('up')
+    //console.log('up')
+})
+
+downBtn.addEventListener('click', ()=>{
+    changeSlide('down')
+})
+
+document.addEventListener('keydown', event=>{
+    changeSlide(event.key)
+})
+
+function changeSlide(direction){
+    if(direction === 'up' || direction === 'ArrowUp'){
+        activeSlideIndex++
+        //console.log(activeSlideIndex)
+        if(activeSlideIndex===slidesCount){
+            activeSlideIndex = 0
+        }
+    }else if(direction === 'down' || direction === 'ArrowDown'){
+        activeSlideIndex--
+        if(activeSlideIndex < 0){
+            activeSlideIndex = slidesCount-1
+        }
+    }
+
+    const height = container.clientHeight
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex*height}px)`
+    sidebar.style.transform = `translateY(${activeSlideIndex*height}px)`
+}
+
